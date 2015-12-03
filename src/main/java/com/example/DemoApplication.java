b3c73e8caf1a3964e2db5815cdf426e1174430f2
Alexey Dushen <dushen.alexey@gmail.com>
2015-12-04 00:46:49 +0300

    add a web controller

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,21 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
